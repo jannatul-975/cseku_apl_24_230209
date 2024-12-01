e3c87cbf05b24693a22b43cb5538af71bc3db0ae
Jannatul Ferdousi Riti <jannatulferdousi97500@gmail.com>
2024-12-01 17:10:35 +0600

    Update task1

--- task1/RemoveSpace_new.c
+++ task1/RemoveSpace_new.c
@@ -3,33 +3,33 @@
 
 int main()
 {
-    int i, j;     // Loop counters
-    char str[50]; // Array to hold the input string
+    int outerIndex, innerIndex;     // Loop counters
+    char inputString[50]; // Array to hold the input string
 
     // Prompt user for input
     printf("Enter a string: ");
-    gets(str); // Read the input string
+    gets(inputString); // Read the input string
 
-    int l = strlen(str); // Get the length of the string
+    int length = strlen(inputString); // Get the length of the string
 
     // Loop through the string
-    for (i = 0; i < l; i++)
+    for (outerIndex = 0; outerIndex < length; outerIndex++)
     {
         // Check for spaces in the string
-        if (str[i] == ' ')
+        if (inputString[outerIndex] == ' ')
         {
             // Shift characters to the left to remove the space
-            for (j = i; j < l; j++)
+            for (innerIndex = outerIndex; innerIndex < length; innerIndex++)
             {
-                str[j] = str[j + 1];
+                inputString[innerIndex] = inputString[innerIndex + 1];
             }
-            l--; // Decrease length since we removed a character
-            i--; // Decrease index to check the new character at the same position
+            length--; // Decrease length since we removed a character
+            outerIndex--; // Decrease index to check the new character at the same position
         }
     }
 
     // Output the modified string without spaces
-    puts(str);
+    puts(inputString);
     return 0;
 }
 /*
