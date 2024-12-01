5fd2631850612860eed0fc393492d944046ebed8
Jannatul Ferdousi Riti <jannatulferdousi97500@gmail.com>
2024-12-01 17:09:51 +0600

    Update task1

--- task1/Prime_new.c
+++ task1/Prime_new.c
@@ -3,16 +3,16 @@
 
 int main(void)
 {
-    int n, flag = 1; // Decleration of variables
+    int number, flag = 1; // Decleration of variables
 
     // Prompt the user to enter a number
     printf("Enter a number: ");
-    scanf("%d", &n); // Scan a number
+    scanf("%d", &number); // Scan a number
 
     // Check for factors from 2 to sqrt(n)
-    for (int i = 2; i <= sqrt(n); i++)
+    for (int index = 2; index <= sqrt(number); index++)
     {
-        if (n % i == 0 && n != 2)
+        if (number % index == 0 && number != 2)
         {
             flag = 0; // Set flag to 0 if a factor is found
             break;    // Exit the loop if a factor is found
@@ -20,21 +20,21 @@ int main(void)
     }
 
     // Special case for number 1
-    if (n == 1)
+    if (number == 1)
     {
-        printf("The number %d is not prime", n);
+        printf("The number %d is not prime", number);
     }
 
     // If no factors were found, the number is prime
     else if (flag == 1)
     {
-        printf("The number %d is prime", n);
+        printf("The number %d is prime", number);
     }
 
     // If a factor was found, the number is not prime
     else
     {
-        printf("The number %d is not prime", n);
+        printf("The number %d is not prime", number);
     }
 
     return 0;
