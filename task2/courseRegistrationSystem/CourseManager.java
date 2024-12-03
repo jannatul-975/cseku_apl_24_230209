fb51f0916ab7dc21b7ea0714782e07dea5422c32
Jannatul Ferdousi Riti <jannatulferdousi97500@gmail.com>
2024-12-03 17:37:15 +0600

    Update task2

--- task2/courseRegistrationSystem/CourseManager.java
+++ task2/courseRegistrationSystem/CourseManager.java
@@ -2,7 +2,10 @@ package courseRegistrationSystem;
 
 public interface CourseManager {
 
+    //Method for add a new course
     void createCourse(Course course);
+
+    //Method for remove a course
     void deleteCourse(Course course);
 
 }
