2cb3837d09abfed6884af2d19f85e0290b2603f4
Jannatul Ferdousi Riti <jannatulferdousi97500@gmail.com>
2024-12-03 17:50:16 +0600

    Update task2

--- task2/courseRegistrationSystem/Instructor.java
+++ task2/courseRegistrationSystem/Instructor.java
@@ -11,19 +11,30 @@ public class Instructor extends Person implements CourseManager {
         this.managedCourses = new ArrayList<>();
     }
 
-
+    //Create a new course
     public void createCourse(Course course) {
 
     }
 
-
+    //Delete an existing course
     public void deleteCourse(Course course) {
 
     }
 
+    //To change course attributes
+    public void setManagedCourses() {
 
+    }
+
+    //Get list of managed courses
     public List<Course> getManagedCourses() {
+        //Implement logic for managing cources
+        //
+        return managedCourses;
+    }
 
+    public String toString() {
+        return super.toString();
     }
 }
 
