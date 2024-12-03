3075f5c225fe0a096f5969b35810f1f4faf90759
Jannatul Ferdousi Riti <jannatulferdousi97500@gmail.com>
2024-12-03 18:00:40 +0600

    Update task2

--- task2/courseRegistrationSystem/Person.java
+++ task2/courseRegistrationSystem/Person.java
@@ -11,28 +11,38 @@ public class Person {
         this.email = email;
     }
 
+    //Get ID
     public String getId() {
         return id;
     }
 
+    //Get name
     public String getName() {
         return name;
     }
 
+    //Get email
     public String getEmail() {
         return email;
     }
 
+    //Set ID
     public void setId(String id) {
         this.id = id;
     }
 
+    //Set name
     public void setName(String name) {
         this.name = name;
     }
 
+    //Set email
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String toString() {
+        return "\nId:"+id+"\nName: "+name+"\nEmail"+email;
     }
 }
 
